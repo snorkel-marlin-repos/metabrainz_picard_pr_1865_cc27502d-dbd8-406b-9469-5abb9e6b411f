a151b7ebf51fa6fc57821a023bf5aa4a63ddd9d8
Bob Swift <bswift@rsds.ca>
2021-07-09 09:54:44 -0600

    Correct link for option profiles help

---
+++ picard/ui/options/dialog.py
@@ -0,0 +1,265 @@
+# -*- coding: utf-8 -*-
+#
+# Picard, the next-generation MusicBrainz tagger
+#
+# Copyright (C) 2006-2008, 2011 Lukáš Lalinský
+# Copyright (C) 2008-2009 Nikolai Prokoschenko
+# Copyright (C) 2008-2009, 2018-2021 Philipp Wolfer
+# Copyright (C) 2011 Pavan Chander
+# Copyright (C) 2011-2012, 2019 Wieland Hoffmann
+# Copyright (C) 2011-2013 Michael Wiencek
+# Copyright (C) 2013, 2017-2021 Laurent Monin
+# Copyright (C) 2014 Sophist-UK
+# Copyright (C) 2016-2017 Sambhav Kothari
+# Copyright (C) 2017 Suhas
+# Copyright (C) 2018 Vishal Choudhary
+# Copyright (C) 2021 Bob Swift
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+
+from PyQt5 import (
+    QtCore,
+    QtWidgets,
+)
+
+from picard import log
+from picard.config import (
+    ListOption,
+    TextOption,
+    get_config,
+)
+from picard.util import restore_method
+
+from picard.ui import (
+    HashableTreeWidgetItem,
+    PicardDialog,
+    SingletonDialog,
+)
+from picard.ui.options import (  # noqa: F401 # pylint: disable=unused-import
+    OptionsCheckError,
+    _pages as page_classes,
+    advanced,
+    cdlookup,
+    cover,
+    fingerprinting,
+    general,
+    genres,
+    interface,
+    interface_colors,
+    interface_top_tags,
+    matching,
+    metadata,
+    network,
+    plugins,
+    ratings,
+    releases,
+    renaming,
+    scripting,
+    tags,
+    tags_compatibility_aac,
+    tags_compatibility_ac3,
+    tags_compatibility_id3,
+    tags_compatibility_wave,
+)
+from picard.ui.util import StandardButton
+
+
+class OptionsDialog(PicardDialog, SingletonDialog):
+
+    options = [
+        TextOption("persist", "options_last_active_page", ""),
+        ListOption("persist", "options_pages_tree_state", []),
+    ]
+
+    def add_pages(self, parent, default_page, parent_item):
+        pages = [(p.SORT_ORDER, p.NAME, p) for p in self.pages if p.PARENT == parent]
+        items = []
+        for foo, bar, page in sorted(pages):
+            item = HashableTreeWidgetItem(parent_item)
+            item.setText(0, _(page.TITLE))
+            if page.ACTIVE:
+                self.item_to_page[item] = page
+                self.page_to_item[page.NAME] = item
+                self.ui.pages_stack.addWidget(page)
+            else:
+                item.setFlags(QtCore.Qt.ItemIsEnabled)
+            self.add_pages(page.NAME, default_page, item)
+            if page.NAME == default_page:
+                self.default_item = item
+            items.append(item)
+        if not self.default_item and not parent:
+            self.default_item = items[0]
+
+    def __init__(self, default_page=None, parent=None):
+        super().__init__(parent)
+        self.setWindowModality(QtCore.Qt.ApplicationModal)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+
+        from picard.ui.ui_options import Ui_Dialog
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+
+        self.ui.reset_all_button = QtWidgets.QPushButton(_("&Restore all Defaults"))
+        self.ui.reset_all_button.setToolTip(_("Reset all of Picard's settings"))
+        self.ui.reset_button = QtWidgets.QPushButton(_("Restore &Defaults"))
+        self.ui.reset_button.setToolTip(_("Reset all settings for current option page"))
+
+        ok = StandardButton(StandardButton.OK)
+        ok.setText(_("Make It So!"))
+        self.ui.buttonbox.addButton(ok, QtWidgets.QDialogButtonBox.AcceptRole)
+        self.ui.buttonbox.addButton(StandardButton(StandardButton.CANCEL), QtWidgets.QDialogButtonBox.RejectRole)
+        self.ui.buttonbox.addButton(StandardButton(StandardButton.HELP), QtWidgets.QDialogButtonBox.HelpRole)
+        self.ui.buttonbox.addButton(self.ui.reset_all_button, QtWidgets.QDialogButtonBox.ActionRole)
+        self.ui.buttonbox.addButton(self.ui.reset_button, QtWidgets.QDialogButtonBox.ActionRole)
+
+        self.ui.buttonbox.accepted.connect(self.accept)
+        self.ui.buttonbox.rejected.connect(self.reject)
+        self.ui.reset_all_button.clicked.connect(self.confirm_reset_all)
+        self.ui.reset_button.clicked.connect(self.confirm_reset)
+        self.ui.buttonbox.helpRequested.connect(self.show_help)
+
+        self.pages = []
+        for Page in page_classes:
+            try:
+                page = Page(self.ui.pages_stack)
+                self.pages.append(page)
+            except Exception:
+                log.exception('Failed initializing options page %r', page)
+        self.item_to_page = {}
+        self.page_to_item = {}
+        self.default_item = None
+        if not default_page:
+            config = get_config()
+            default_page = config.persist["options_last_active_page"]
+        self.add_pages(None, default_page, self.ui.pages_tree)
+
+        # work-around to set optimal option pane width
+        self.ui.pages_tree.expandAll()
+        max_page_name = self.ui.pages_tree.sizeHintForColumn(0) + 2*self.ui.pages_tree.frameWidth()
+        self.ui.dialog_splitter.setSizes([max_page_name, self.geometry().width() - max_page_name])
+
+        self.ui.pages_tree.setHeaderLabels([""])
+        self.ui.pages_tree.header().hide()
+        self.ui.pages_tree.itemSelectionChanged.connect(self.switch_page)
+
+        self.restoreWindowState()
+        self.finished.connect(self.saveWindowState)
+
+        for page in self.pages:
+            try:
+                page.load()
+            except Exception:
+                log.exception('Failed loading options page %r', page)
+                self.disable_page(page.NAME)
+        self.ui.pages_tree.setCurrentItem(self.default_item)
+
+    def switch_page(self):
+        items = self.ui.pages_tree.selectedItems()
+        if items:
+            config = get_config()
+            page = self.item_to_page[items[0]]
+            config.persist["options_last_active_page"] = page.NAME
+            self.ui.pages_stack.setCurrentWidget(page)
+
+    def disable_page(self, name):
+        item = self.page_to_item[name]
+        item.setDisabled(True)
+
+    @property
+    def help_url(self):
+        current_page = self.ui.pages_stack.currentWidget()
+        url = current_page.HELP_URL
+        # If URL is empty, use the first non empty parent help URL.
+        while current_page.PARENT and not url:
+            current_page = self.item_to_page[self.page_to_item[current_page.PARENT]]
+            url = current_page.HELP_URL
+        if not url:
+            url = 'doc_options'  # key in PICARD_URLS
+        return url
+
+    def accept(self):
+        for page in self.pages:
+            try:
+                page.check()
+            except OptionsCheckError as e:
+                self._show_page_error(page, e)
+                return
+            except Exception as e:
+                log.exception('Failed checking options page %r', page)
+                self._show_page_error(page, e)
+                return
+        for page in self.pages:
+            try:
+                page.save()
+            except Exception as e:
+                log.exception('Failed saving options page %r', page)
+                self._show_page_error(page, e)
+                return
+        super().accept()
+
+    def _show_page_error(self, page, error):
+        if not isinstance(error, OptionsCheckError):
+            error = OptionsCheckError(_('Unexpected error'), str(error))
+        self.ui.pages_tree.setCurrentItem(self.page_to_item[page.NAME])
+        page.display_error(error)
+
+    def saveWindowState(self):
+        expanded_pages = []
+        for page, item in self.page_to_item.items():
+            index = self.ui.pages_tree.indexFromItem(item)
+            is_expanded = self.ui.pages_tree.isExpanded(index)
+            expanded_pages.append((page, is_expanded))
+        config = get_config()
+        config.persist["options_pages_tree_state"] = expanded_pages
+
+    @restore_method
+    def restoreWindowState(self):
+        config = get_config()
+        pages_tree_state = config.persist["options_pages_tree_state"]
+        if not pages_tree_state:
+            self.ui.pages_tree.expandAll()
+        else:
+            for page, is_expanded in pages_tree_state:
+                try:
+                    item = self.page_to_item[page]
+                except KeyError:
+                    continue
+                item.setExpanded(is_expanded)
+
+    def restore_all_defaults(self):
+        for page in self.pages:
+            page.restore_defaults()
+
+    def restore_page_defaults(self):
+        self.ui.pages_stack.currentWidget().restore_defaults()
+
+    def confirm_reset(self):
+        msg = _("You are about to reset your options for this page.")
+        self._show_dialog(msg, self.restore_page_defaults)
+
+    def confirm_reset_all(self):
+        msg = _("Warning! This will reset all of your settings.")
+        self._show_dialog(msg, self.restore_all_defaults)
+
+    def _show_dialog(self, msg, function):
+        message_box = QtWidgets.QMessageBox(self)
+        message_box.setIcon(QtWidgets.QMessageBox.Warning)
+        message_box.setWindowModality(QtCore.Qt.WindowModal)
+        message_box.setWindowTitle(_("Confirm Reset"))
+        message_box.setText(_("Are you sure?") + "\n\n" + msg)
+        message_box.setStandardButtons(QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
+        if message_box.exec_() == QtWidgets.QMessageBox.Yes:
+            function()
---
+++ picard/ui/options/scripting.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+#
+# Picard, the next-generation MusicBrainz tagger
+#
+# Copyright (C) 2006-2007, 2009 Lukáš Lalinský
+# Copyright (C) 2009 Nikolai Prokoschenko
+# Copyright (C) 2009-2010, 2019-2021 Philipp Wolfer
+# Copyright (C) 2011-2013 Michael Wiencek
+# Copyright (C) 2013-2015, 2017-2020 Laurent Monin
+# Copyright (C) 2014 m42i
+# Copyright (C) 2016-2017 Sambhav Kothari
+# Copyright (C) 2016-2017 Suhas
+# Copyright (C) 2018 Vishal Choudhary
+# Copyright (C) 2021 Bob Swift
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+
+from PyQt5 import QtCore
+
+from picard.config import (
+    BoolOption,
+    IntOption,
+    ListOption,
+    get_config,
+)
+from picard.const.sys import IS_MACOS
+from picard.script import ScriptParser
+
+from picard.ui import (
+    PicardDialog,
+    SingletonDialog,
+)
+from picard.ui.moveable_list_view import MoveableListView
+from picard.ui.options import (
+    OptionsCheckError,
+    OptionsPage,
+    register_options_page,
+)
+from picard.ui.ui_options_script import Ui_ScriptingOptionsPage
+from picard.ui.ui_scripting_documentation_dialog import (
+    Ui_ScriptingDocumentationDialog,
+)
+from picard.ui.widgets.scriptdocumentation import ScriptingDocumentationWidget
+from picard.ui.widgets.scriptlistwidget import ScriptListWidgetItem
+
+
+class ScriptCheckError(OptionsCheckError):
+    pass
+
+
+class ScriptingDocumentationDialog(PicardDialog, SingletonDialog):
+    defaultsize = QtCore.QSize(570, 400)
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        # on macOS having this not a dialog causes the window to be placed
+        # behind the options dialog.
+        if not IS_MACOS:
+            self.setWindowFlags(QtCore.Qt.Window)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        self.parent = parent
+        self.ui = Ui_ScriptingDocumentationDialog()
+        self.ui.setupUi(self)
+        doc_widget = ScriptingDocumentationWidget(self)
+        self.ui.documentation_layout.addWidget(doc_widget)
+        self.ui.buttonBox.rejected.connect(self.close)
+
+    def closeEvent(self, event):
+        super().closeEvent(event)
+
+
+class ScriptingOptionsPage(OptionsPage):
+
+    NAME = "scripting"
+    TITLE = N_("Scripting")
+    PARENT = None
+    SORT_ORDER = 85
+    ACTIVE = True
+    HELP_URL = '/config/options_scripting.html'
+
+    options = [
+        BoolOption("setting", "enable_tagger_scripts", False),
+        ListOption("setting", "list_of_scripts", []),
+        IntOption("persist", "last_selected_script_pos", 0),
+    ]
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_ScriptingOptionsPage()
+        self.ui.setupUi(self)
+        self.ui.tagger_script.setEnabled(False)
+        self.ui.scripting_options_splitter.setStretchFactor(1, 2)
+        self.move_view = MoveableListView(self.ui.script_list, self.ui.move_up_button,
+                                          self.ui.move_down_button)
+        self.ui.scripting_documentation_button.clicked.connect(self.show_scripting_documentation)
+
+    def show_scripting_documentation(self):
+        ScriptingDocumentationDialog.show_instance(parent=self)
+
+    def enable_tagger_scripts_toggled(self, on):
+        if on and self.ui.script_list.count() == 0:
+            self.ui.script_list.add_script()
+
+    def script_selected(self):
+        items = self.ui.script_list.selectedItems()
+        if items:
+            item = items[0]
+            self.ui.tagger_script.setEnabled(True)
+            self.ui.tagger_script.setText(item.script)
+            self.ui.tagger_script.setFocus(QtCore.Qt.OtherFocusReason)
+        else:
+            self.ui.tagger_script.setEnabled(False)
+            self.ui.tagger_script.setText("")
+
+    def live_update_and_check(self):
+        items = self.ui.script_list.selectedItems()
+        if items:
+            script = items[0]
+            script.script = self.ui.tagger_script.toPlainText()
+        self.ui.script_error.setStyleSheet("")
+        self.ui.script_error.setText("")
+        try:
+            self.check()
+        except OptionsCheckError as e:
+            self.ui.script_error.setStyleSheet(self.STYLESHEET_ERROR)
+            self.ui.script_error.setText(e.info)
+            return
+
+    def check(self):
+        parser = ScriptParser()
+        try:
+            parser.eval(self.ui.tagger_script.toPlainText())
+        except Exception as e:
+            raise ScriptCheckError(_("Script Error"), str(e))
+
+    def restore_defaults(self):
+        # Remove existing scripts
+        self.ui.script_list.clear()
+        self.ui.tagger_script.setText("")
+        super().restore_defaults()
+
+    def load(self):
+        config = get_config()
+        self.ui.enable_tagger_scripts.setChecked(config.setting["enable_tagger_scripts"])
+        for pos, name, enabled, text in config.setting["list_of_scripts"]:
+            list_item = ScriptListWidgetItem(name, enabled, text)
+            self.ui.script_list.addItem(list_item)
+
+        # Select the last selected script item
+        last_selected_script_pos = config.persist["last_selected_script_pos"]
+        last_selected_script = self.ui.script_list.item(last_selected_script_pos)
+        if last_selected_script:
+            self.ui.script_list.setCurrentItem(last_selected_script)
+            last_selected_script.setSelected(True)
+
+    def _all_scripts(self):
+        for row in range(0, self.ui.script_list.count()):
+            item = self.ui.script_list.item(row)
+            yield item.get_all()
+
+    def save(self):
+        config = get_config()
+        config.setting["enable_tagger_scripts"] = self.ui.enable_tagger_scripts.isChecked()
+        config.setting["list_of_scripts"] = list(self._all_scripts())
+        config.persist["last_selected_script_pos"] = self.ui.script_list.currentRow()
+
+    def display_error(self, error):
+        # Ignore scripting errors, those are handled inline
+        if not isinstance(error, ScriptCheckError):
+            super().display_error(error)
+
+
+register_options_page(ScriptingOptionsPage)
